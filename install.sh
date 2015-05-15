fea1314c8062c5f15004623e103a8e00555ba7a7
dethtron5000 <polson@ideo.com>
2015-05-15 17:30:12 -0400

    adding install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "\033[31;1mHI!\033[0m"
+echo "\033[1mThis script will install Dragonfruit\033[0m."
+TDIR="._tmp"
+mkdir -p $TDIR
+HOMEDIR=$(pwd)
+cd $HOMEDIR
+
+echo "\033[31;1mFirst, we'll uninstall any old versions of Dragonfruit you might have.\033[0m."
+brew uninstall dragonfruit
+
+# set up dragonfruit
+echo "\n\n"
+cd $TDIR
+git clone "https://github.com/ideo/homebrew.git"
+cd "homebrew/dragonfruit"
+brew install "dragonfruit.rb"
+
+cd $HOMEDIR
+rm -rf $TDIR
