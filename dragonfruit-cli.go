88bc72d7a65cca251df7796078a642d06803703e
Peter Olson <peter@overtype.com>
2017-10-30 22:53:18 -0400

    removing oAuth stuff

--- dragonfruit-cli.go
+++ dragonfruit-cli.go
@@ -13,18 +13,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/ideo/dragonfruit/backends/backend_couchdb"
 	"github.com/martini-contrib/gzip"
-	"github.com/martini-contrib/oauth2"
 	"github.com/martini-contrib/sessions"
-	goauth2 "golang.org/x/oauth2"
 )
 
 const (
 	VERSION = "0.5.5"
+        PATH_TO_DEFAULT_CONFIG = "/usr/local/etc/dragonfruit.conf"
 )
 
 type cnf struct {
@@ -36,27 +33,11 @@ type cnf struct {
 	resourcefile   string
 }
 
-type loginresult struct {
-	User       gplususer `json:"user"`
-	IsLoggedIn bool      `json:"isLoggedIn"`
-}
-
-type gplususer struct {
-	Emails []struct {
-		Value string `json:"value"`
-		Type  string `json:"type"`
-	} `json:"emails"`
-	DisplayName string `json:"displayName"`
-	Image       struct {
-		Url string `json:"url"`
-	} `json:"image"`
-	Expires time.Time `json:"expires"`
-}
-
+/* main executable function */
 func main() {
 	fmt.Println("\n\n\033[31m~~~~~Dragon\033[32mFruit~~~~~\033[0m")
 	cnf := parseFlags()
-	fmt.Print("\033[1mVersion", VERSION, " \033[0m\n\n\n")
+	fmt.Print("\033[1mVersion ", VERSION, " \033[0m\n\n\n")
 	if cnf.version {
 		return
 	}
@@ -100,43 +81,6 @@ func launchServer(d dragonfruit.Db_backend, cnf dragonfruit.Conf) *martini.Class
 
 	m.Use(gzip.All())
 
-	oauthConf := &goauth2.Config{
-		ClientID:     "288198830216-s4klktd4qm3asq72sm7acifugcumdseq.apps.googleusercontent.com",
-		ClientSecret: "zzKEh5RPFhaEeJOsQi-D34Rc",
-		Scopes: []string{
-			"https://www.googleapis.com/auth/userinfo.email"},
-		RedirectURL: "http://" + cnf.Host + ":" + cnf.Port + "/oauth2callback",
-	}
-	m.Use(oauth2.Google(oauthConf))
-	m.Post("/checkauth", oauth2.LoginRequired, func(tokens oauth2.Tokens, res http.ResponseWriter, s sessions.Session) (int, string) {
-		h := res.Header()
-		h.Add("Content-Type", "application/json; charset=utf-8")
-
-		code := 200
-
-		result := loginresult{}
-
-		if tokens.Expired() {
-			result.IsLoggedIn = false
-			code = 403
-		}
-
-		test, user := checkAuth(oauthConf, tokens)
-
-		if !test {
-			result.IsLoggedIn = false
-			code = 403
-		} else {
-			result.IsLoggedIn = true
-			result.User = user
-		}
-		out, err := json.Marshal(result)
-		if err != nil {
-			code = 500
-		}
-		return code, string(out)
-	})
-
 	m.RunOnAddr(cnf.Host + ":" + cnf.Port)
 	return m
 }
@@ -149,39 +93,8 @@ func returnSuccess(res http.ResponseWriter) (int, string) {
 	return 200, "auth"
 }
 
-func checkAuth(conf *goauth2.Config, tokens oauth2.Tokens) (bool, gplususer) {
-	var user gplususer
-	//fmt.Println(tokens)
-	tok := &goauth2.Token{
-		AccessToken:  tokens.Access(),
-		Expiry:       tokens.ExpiryTime(),
-		RefreshToken: tokens.Refresh(),
-	}
-
-	client := conf.Client(goauth2.NoContext, tok)
-	resp, err := client.Get("https://www.googleapis.com/plus/v1/people/me")
-	defer resp.Body.Close()
-
-	if err != nil {
-		return false, user
-	}
-
-	out, err := ioutil.ReadAll(resp.Body)
-
-	json.Unmarshal(out, &user)
-
-	for _, v := range user.Emails {
-		if strings.Contains(v.Value, "ideo.com") {
-			user.Expires = tokens.ExpiryTime()
-			return true, user
-		}
-	}
-
-	return false, user
-}
-
-/* addResourceFromFile adds a new resource directly from a file, with the
-standard naming conventions */
+/* addResourceFromFile adds a new resource directly from a file, with
+standard naming conventions for resources and API endpoints */
 func addResourceFromFile(d dragonfruit.Db_backend,
 	cnf dragonfruit.Conf,
 	resourceType string,
@@ -199,6 +112,7 @@ func addResourceFromFile(d dragonfruit.Db_backend,
 
 }
 
+/* addResource handles interactive mode parsing and naming of resources */
 func addResouce(d dragonfruit.Db_backend, cnf dragonfruit.Conf) {
 
 	// start a scanner to read from the command line
@@ -236,6 +150,7 @@ func addResouce(d dragonfruit.Db_backend, cnf dragonfruit.Conf) {
 
 }
 
+/* parseFlags parses the command-line flags passed to the CLI */
 func parseFlags() cnf {
 
 	// set up a config object
@@ -256,10 +171,11 @@ func parseFlags() cnf {
 	/* If we do parse a file, what is the resource type for that file? */
 	var resourcetype = flag.String("type", "", "The resource type for the file.")
 
+	/* If not using the default config file, path to a config file */
 	var conflocation = flag.String("conf", "", "Path to a config file.")
 
 	flag.Parse()
-	baseConf, err := ioutil.ReadFile("/usr/local/etc/dragonfruit.conf")
+	baseConf, err := ioutil.ReadFile(PATH_TO_DEFAULT_CONFIG)
 	if err != nil {
 		panic("base conf file missing")
 	}
